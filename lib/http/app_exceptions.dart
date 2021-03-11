05ea65651566fb2e3c3c71086be2acf776094927
保锋 <iambaofeng@gmail.com>
2021-03-11 18:03:37 +0800

    删除break

--- lib/http/app_exceptions.dart
+++ lib/http/app_exceptions.dart
@@ -20,22 +20,22 @@ class AppException implements Exception {
         {
           return BadRequestException(-1, "请求取消");
         }
-        break;
+
       case DioErrorType.CONNECT_TIMEOUT:
         {
           return BadRequestException(-1, "连接超时");
         }
-        break;
+
       case DioErrorType.SEND_TIMEOUT:
         {
           return BadRequestException(-1, "请求超时");
         }
-        break;
+
       case DioErrorType.RECEIVE_TIMEOUT:
         {
           return BadRequestException(-1, "响应超时");
         }
-        break;
+
       case DioErrorType.RESPONSE:
         {
           try {
@@ -47,47 +47,47 @@ class AppException implements Exception {
                 {
                   return BadRequestException(errCode, "请求语法错误");
                 }
-                break;
+
               case 401:
                 {
                   return UnauthorisedException(errCode, "没有权限");
                 }
-                break;
+
               case 403:
                 {
                   return UnauthorisedException(errCode, "服务器拒绝执行");
                 }
-                break;
+
               case 404:
                 {
                   return UnauthorisedException(errCode, "无法连接服务器");
                 }
-                break;
+
               case 405:
                 {
                   return UnauthorisedException(errCode, "请求方法被禁止");
                 }
-                break;
+
               case 500:
                 {
                   return UnauthorisedException(errCode, "服务器内部错误");
                 }
-                break;
+
               case 502:
                 {
                   return UnauthorisedException(errCode, "无效的请求");
                 }
-                break;
+
               case 503:
                 {
                   return UnauthorisedException(errCode, "服务器挂了");
                 }
-                break;
+
               case 505:
                 {
                   return UnauthorisedException(errCode, "不支持HTTP协议请求");
                 }
-                break;
+
               default:
                 {
                   // return ErrorEntity(code: errCode, message: "未知错误");
@@ -98,7 +98,7 @@ class AppException implements Exception {
             return AppException(-1, "未知错误");
           }
         }
-        break;
+
       default:
         {
           return AppException(-1, error.message);
